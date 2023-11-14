98d22ce54ff7d8392d6c328d793962b5e7618a06
Aniket Aryamane <aryamane.aniket@gmail.com>
2023-11-14 23:28:44 +0530

    Cron job - keep server alive

---
+++ cron.js
@@ -0,0 +1,26 @@
+const cron = require("cron");
+const https = require("https");
+
+const backendURL = "https://everearth.onrender.com/";
+
+const job = new cron.CronJob("*/14 * * * *", function () {
+    console.log("Restarting Server");
+
+    https
+        .get(backendURL, (res) => {
+            if (res.statusCode === 200) {
+                console.log("Server started");
+            } else {
+                console.error(
+                    `Failed to restart server with status code: ${res.statusCode}`
+                );
+            }
+        })
+        .on("error", (err) => {
+            console.log("Error during restart: ", err.message);
+        });
+});
+
+module.exports = {
+    job,
+};
--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; //  chose port from here like 8080, 3001
+const job = require("~/cron.js");
 
+job.start();
 server.use(middlewares);
 server.use(router);
 
